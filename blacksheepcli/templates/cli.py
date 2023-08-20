fb3896f6c625309c243335954166d0b4027c84e1
Roberto Prevato <roberto.prevato@gmail.com>
2023-08-20 13:24:50 +0200

    Add support for folders and SSH clone

--- blacksheepcli/templates/cli.py
+++ blacksheepcli/templates/cli.py
@@ -1,4 +1,5 @@
 from json import dumps
+from typing import Optional
 
 import questionary
 
@@ -21,6 +22,18 @@ def templates():
     """
 
 
+def _display_source_details(template: Template):
+    from rich.text import Text
+
+    text = Text(template.source)
+
+    if template.tag:
+        text.append(f"\ntag: {template.tag}", style="yellow")
+    if template.folder:
+        text.append(f"\nfolder: {template.folder}", style="yellow")
+    return text
+
+
 @click.command(name="details")
 def describe_templates():
     """
@@ -38,14 +51,16 @@ def describe_templates():
     table.add_column("Description", justify="left", style="green")
 
     for template in templates:
-        table.add_row(template.id, template.source, template.description)
+        table.add_row(
+            template.id, _display_source_details(template), template.description
+        )
 
     console = Console()
     console.print(table)
 
 
 @click.command(name="list")
-@click.option("--source", "-s", is_flag=True, help="Include the source in the output.s")
+@click.option("--source", "-s", is_flag=True, help="Include the source in the output.")
 def list_templates(source: bool):
     """
     Lists all available templates.
@@ -63,6 +78,7 @@ def list_templates(source: bool):
 @click.command(name="add")
 @click.argument("name")
 @click.argument("source")
+@click.argument("folder", required=False)
 @click.option(
     "-d",
     "--description",
@@ -71,11 +87,13 @@ def list_templates(source: bool):
     help="Template description.",
 )
 @click.option("-f", "--force", is_flag=True, help="Force update if the template exists")
-def add_template(name: str, source: str, description: str, force: bool):
+def add_template(
+    name: str, source: str, folder: Optional[str], description: str, force: bool
+):
     """
     Add a template, by name and source, with optional description. If a specific tag
     should be used for a Git repository, it can be specified at the end of the source,
-    using an "@" sign. Example: https://github.com/Neoteroi/BlackSheep-Foo@v2
+    using an "@" sign. Example: https://github.com/Neoteroi/BlackSheep-Foo$v2
 
     This command fails if a template exists with the same name. To overwrite an existing
     template, use the -f, or --force flag. Templates can later be used to scaffold new
@@ -86,10 +104,10 @@ def add_template(name: str, source: str, description: str, force: bool):
     blacksheep templates add foo https://github.com/Neoteroi/BlackSheep-Foo
         -d 'Some nice template! 🐃'
 
-    blacksheep templates add foo2 https://github.com/Neoteroi/BlackSheepFoo@v2
+    blacksheep templates add foo2 'https://github.com/Neoteroi/BlackSheepFoo$v2'
     """
     manager = TemplatesManager()
-    manager.add_template(name, source, description, force)
+    manager.add_template(name, source, folder, description, force)
 
 
 @click.command(name="remove")
